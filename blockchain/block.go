3ee705b5fd7c6bc4416d5046dfb42b636c825667
Dheeraj K <dyaduwanshi0@gmail.com>
2025-06-05 15:23:08 +0530

    add block related functionality

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,5 +1,37 @@
 package blockchain
 
-func Block() {
-	print("block");
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
+)
+
+// block
+type Block struct {
+	Index 		int			// height of the block
+	Timestamp	time.Time	// time of block creation
+	Data        string		// data
+	PrevHash    string		// hash of prev block
+	Validator   string		// ID of the validator who proposed this block
+	CurrentHash string		// hash of this block (calculated)
+}
+
+// calculate Hash
+func (b *Block) CalculateHash() string {
+	input := string(b.Index) + b.Timestamp.String() + b.Data + b.PrevHash + b.Validator
+	hash := sha256.Sum256([]byte(input))
+	return hex.EncodeToString(hash[:])
+}
+
+// new block
+func NewBlock(index int, data string, prevHash string, validator string) *Block {
+	block := &Block{
+		Index: index,
+		Timestamp: time.Now(),
+		Data: data,
+		PrevHash: prevHash,
+		Validator: validator,
+	}
+	block.CurrentHash = block.CalculateHash()
+	return block
 }
