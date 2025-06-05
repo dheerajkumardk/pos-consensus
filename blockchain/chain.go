d9a4913764b08fc3c6a02ebb22f47c2b2c21f30f
Dheeraj K <dyaduwanshi0@gmail.com>
2025-06-05 15:23:56 +0530

    add blockchain functionalities

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -1,5 +1,41 @@
 package blockchain
 
-func Chain() {
-	print("chain")
+type Blockchain struct {
+	Blocks []*Block
+}
+
+// function
+// create new blockchain -> initializes with genesis block
+func NewBlockchain() *Blockchain {
+	// Create genesis block
+	genesisBlock := NewBlock(0, "", "", "")
+	return &Blockchain{
+		Blocks: []*Block{genesisBlock},
+	}
+}
+
+// add block
+func (bc *Blockchain) AddBlock(data string, validator string) {
+	prevBlock := bc.Blocks[len(bc.Blocks) - 1]
+	newBlock := NewBlock(prevBlock.Index + 1, data, prevBlock.CurrentHash, validator)
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
+// validate chain
+func (bc *Blockchain) ValidateChain() bool {
+	// check prevHash in current block
+	// check currentHash
+	for i := 1; i < len(bc.Blocks); i++ {
+		currentBlock := bc.Blocks[i]
+		prevBlock := bc.Blocks[i-1]
+
+		if prevBlock.CurrentHash != currentBlock.PrevHash {
+			return false
+		}
+
+		if currentBlock.CurrentHash != currentBlock.CalculateHash() {
+			return false
+		}
+	}
+	return true
 }
