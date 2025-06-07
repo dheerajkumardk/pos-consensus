858ba68e8d3aa2bbbdce88ff76c4361727549605
Dheeraj K <dyaduwanshi0@gmail.com>
2025-06-07 12:50:19 +0530

    add validator selection n consensus rules

--- consensus/selection.go
+++ consensus/selection.go
@@ -1,1 +1,24 @@
 package consensus
+
+import "math/rand"
+
+func SelectValidator(vp *ValidatorPool) string {
+	totalStake := vp.GetTotalStake()
+	target := rand.Intn(totalStake)
+	cumulative := 0
+
+	for _,v := range vp.Validators {
+		cumulative += v.Stake
+		if cumulative >= target {
+			return v.ID
+		}
+	}
+	return ""
+}
+
+// Block proposal and validation flow
+// - leader: creates a new block
+// 	- broadcast it to other validators
+
+// - validators: verify the block
+// 	- send votes
--- consensus/validation.go
+++ consensus/validation.go
@@ -1,1 +1,13 @@
 package consensus
+
+// received votes must be greater than minm required
+func CheckConsensus(votes map[string]bool, minVotes int) bool {
+	yayVotes := 0
+
+	for _, vote := range votes {
+		if vote {
+			yayVotes++
+		}
+	}
+	return yayVotes >= minVotes
+}
