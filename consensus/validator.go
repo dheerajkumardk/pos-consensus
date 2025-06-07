2308c79398746bb654ad3dc4dd6c254735e4c837
Dheeraj K <dyaduwanshi0@gmail.com>
2025-06-07 12:49:52 +0530

    add validator structs n functionalities

--- consensus/validator.go
+++ consensus/validator.go
@@ -1,1 +1,32 @@
 package consensus
+
+type Validator struct {
+	ID		string	// Node Identifier
+	Stake	int		// Staked amount
+}
+
+type ValidatorPool struct {
+	Validators []Validator
+}
+
+// functions
+// addValidator
+// getValidators [TODO: We'll see if they are required]
+// getValidatorById
+
+func NewValidatorPool() *ValidatorPool {
+	return &ValidatorPool{}
+}
+
+func (vp *ValidatorPool) AddValidator(id string, stake int) {
+	vp.Validators = append(vp.Validators, Validator{ID: id, Stake: stake})
+}
+
+// get total stake amts
+func (vp *ValidatorPool) GetTotalStake() int {
+	total := 0
+	for _,v := range vp.Validators {
+		total += v.Stake
+	}
+	return total
+}
