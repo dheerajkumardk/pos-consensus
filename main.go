7a990e0da7a1a225f5d50373e14cbcbdc19014f8
Dheeraj K <dyaduwanshi0@gmail.com>
2025-06-07 12:49:21 +0530

    test add block functionality

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
-import ("github.com/dheerajkumardk/pos-consensus/blockchain"
-//  "github.com/dheerajkumardk/pos-consensus/consensus"
-//  "github.com/dheerajkumardk/pos-consensus/network"
+import (
+	"fmt"
+
+	"github.com/dheerajkumardk/pos-consensus/blockchain"
+	// "github.com/dheerajkumardk/pos-consensus/consensus"
+	// "github.com/dheerajkumardk/pos-consensus/network"
 )
 
 func main() {
 	print("hello!");
 
-	// call package blockchain
-	blockchain.Block();
+	bc := blockchain.NewBlockchain()
 
-	blockchain.Chain();
+	bc.AddBlock("Data 1", "Validator A")
+	bc.AddBlock("Data 2", "Validator B")
+
+	fmt.Printf("Blockchain valid? %t \n", bc.ValidateChain())
+
+	println(bc.Blocks[1].CurrentHash)
+	println(bc.Blocks[2].PrevHash)
 }
